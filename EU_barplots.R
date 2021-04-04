3ff4523ae7544518b14d02dd6adcd7ffae80e980
francesca-git <rosa@ifu.baug.ethz.ch>
2021-04-04 09:09:32 +0200

    European internal impacts (conversion from .Rdata to .csv) and barplots
    This version has been saved because afterwads some modification have been implemented for the SETAC presentation

---
+++ EU_barplots.R
@@ -0,0 +1,182 @@
+
+### 1) EU FOOTPRINT - BARPLOT
+### 2) IMPACTS OF EU INTERNAL FORESTS - BARPLOT
+
+
+#############################################################################################################################################################################
+
+# WARNING: The functions defined in this file are called in the file csv_and_plotting.R. The loading/saving paths and the working directory are defined in that file.
+
+#############################################################################################################################################################################
+
+
+library(ggplot2)
+library(colorspace)
+library(tidyverse)
+library(dplyr)
+library(scico)
+library(viridis)
+library(RColorBrewer)
+library(gridExtra)
+library(nord)
+library(ggpubr)
+library(stringr)
+select <- dplyr::select
+  
+
+#############################################################################################################################################################################
+
+                                                                  # 1) EU FOOTPRINT - BARPLOT #
+
+#############################################################################################################################################################################
+
+# General task: plot a figure with four barplotS (one for each combination of climate mitigation scenario and forest use scenario) 
+# Date: September 2020
+# Author: Francesca Rosa
+
+  EUfootprint.barplot <- function(csv_path, case_subcase, plots_path, year) {
+    
+      # folder_slost = folder where the files to be loaded are stored, file_slost = initial part of the name of the file to be loaded, 
+      # case_subcase = identification words which selects the file, 
+      # plots_path = folder where the plots will be saved
+
+      data <-read.csv(paste0(csv_path, "EUFootprint_", year, case_subcase, ".csv"), header = TRUE)
+      #data <- read.csv(paste0("./plotting/cutoff_timber/EUFootprint_", year, "_mg-det_cutoff_timber.csv"), header = TRUE)
+      
+          
+      # choose the palette
+      palette_name = "Terrain 2"
+      #palette_name = "bamako"
+      
+      # arrange the data ====
+      data <- data %>% mutate(Category = str_replace(Category, "EU28_Energy_crops", "EU28 Energy crops"), 
+                              Category = str_replace(Category, "EU28_Forest_net", "EU28 Forests (for internal use)"),
+                              Category = str_replace(Category, "Import_Energy_plantations", "Import Energy plantations"), 
+                              Category = str_replace(Category, "Import_Forest", "Import Forests"))
+      # set the order of the categories
+      data$Category <- factor(data$Category, levels = c("EU28 Energy crops", "Import Energy plantations", "EU28 Forests (for internal use)", "Import Forests"))
+      # factorize the forest management scenarios and rename the elements 
+      data$Scenario <- factor(data$Scenario, levels=unique(data$Scenario))
+      label_scenario <- c("no AFM", "AFM-free", "AFM 25%", "AFM 50%", "AFM 75%", "AFM 100%")  
+      # rename the groups
+      data <- data %>% mutate(Group = str_replace(Group, "REF_MFM", "RCP6.5 (REF) - Multifunctional"), Group = str_replace(Group, "REF_SFM", "RCP6.5 (REF) - Set-aside"),
+                              Group = str_replace(Group, "RCP2.6_MFM", "RCP2.6 - Multifunctional"), Group = str_replace(Group, "RCP2.6_SFM", "RCP2.6 - Set-aside"))
+      # set the order of the groups
+      data$Group <- factor(data$Group, levels = c("RCP6.5 (REF) - Multifunctional", "RCP6.5 (REF) - Set-aside", "RCP2.6 - Multifunctional", "RCP2.6 - Set-aside")) 
+      # ====
+
+      
+      figure <-
+        ggplot(data, aes(x = Scenario, y = PDFx100, fill = Category))+
+        geom_bar(stat='identity', colour = "black", size = 0.3, width = 0.6, position = position_stack(reverse = TRUE))+ # to create a bar plot with a specific width of the bars
+        scale_x_discrete(labels = label_scenario) + # assign the names to the labels
+        theme_minimal(base_size = 13) + # select the theme of the plot
+        theme(legend.position="right", 
+             legend.text = element_text(size = 10),
+             axis.text = element_text(size = 8),
+             axis.text.x = element_text(angle = 90),
+             axis.title = element_text(size = 10),
+             legend.title = element_blank()) +
+        # , ,
+        #ggtitle("Fraction of species loss in 2100 caused by EU forest biomass demand") +
+        xlab("Scenarios") + ylab("PDF%") +
+        #theme(plot.title = element_text(size = 12, face = "bold.italic")) +
+        scale_fill_discrete_sequential(palette = "Terrain 2", c2 = 20) +
+        #scale_fill_scico_d(palette = palette_name, direction = -1) +
+        #scale_fill_manual(values=c("brown1","darkgreen","orange","darkkhaki"))+
+        #theme_bw() +
+        theme(panel.border = element_rect(color="grey", fill=NA)) +
+        facet_wrap(~ Group) 
+      
+        #       strip.text.x = element_text(size = 11), axis.title = element_text(size = 11), 
+        #       ) + #, legend.text = element_text(size = 8), legend.title = element_text(size = 8)) +
+        #labs(fill = "Land-use categ") 
+      
+      figure
+      
+      # plot 
+
+      # save as pdf
+      ggsave(paste0(plots_path, "EUFootprint_", year, "_", palette_name, case_subcase, ".pdf"), width = 20, height = 16, units = "cm")
+      
+      # 
+      # figure <- ggarrange(figure1, figure2, figure3, common.legend = TRUE, legend = "bottom")
+      # 
+      # figure
+      # ggsave(paste0(plots_path, "EUFootprint_", year, "_", palette_name, "_all_taxa.pdf"), width = 20, height = 16, units = "cm")
+  }
+  
+  
+
+
+#############################################################################################################################################################################
+
+                                                                  # 1) EU INTERNAL - BARPLOT #
+
+#############################################################################################################################################################################
+
+# General task: plot a figure with four barplotS (one for each combination of climate mitigation scenario and forest use scenario) 
+# Date: September 2020
+# Author: Francesca Rosa
+
+  
+  EUinternal.barplot <- function(csv_path, case_subcase, plots_path, year) {
+  
+    data<-read.csv(paste0(csv_path, "EUForest_", year, case_subcase, ".csv"), header = TRUE)
+    data_top<-read.csv(paste0(csv_path, "EUForest_", year, case_subcase,"_top.csv"), header = TRUE)
+
+    palette_name = "Greens" # "#238B45","#74C476", "#BAE4B3", "#EDF8E9"
+    #pal = c("#004616", "#238B45","#74C476", "#BAE4B3", "#EDF8E9")
+    
+    #to keep the same order between the scenarios
+    #data_top$Scenario <- factor(marginal_CI$Scenario, levels=unique(marginal_CI$Scenario))
+    data$Scenario <- factor(data$Scenario, levels=unique(data$Scenario))
+    # rename the scenarios
+    label_scenario <- c("no AFM", "AFM-free", "AFM 25%", "AFM 50%", "AFM 75%", "AFM 100%")  
+    # rename the groups
+    data <- data %>% mutate(Group = str_replace(Group, "REF_MFM", "RCP6.5 (REF) - Multifunctional"), Group = str_replace(Group, "REF_SFM", "RCP6.5 (REF) - Set-aside"),
+                            Group = str_replace(Group, "RCP2.6_MFM", "RCP2.6 - Multifunctional"), Group = str_replace(Group, "RCP2.6_SFM", "RCP2.6 - Set-aside"))
+    data_top <- data_top %>% mutate(Group = str_replace(Group, "REF_MFM", "RCP6.5 (REF) - Multifunctional"), Group = str_replace(Group, "REF_SFM", "RCP6.5 (REF) - Set-aside"),
+                             Group = str_replace(Group, "RCP2.6_MFM", "RCP2.6 - Multifunctional"), Group = str_replace(Group, "RCP2.6_SFM", "RCP2.6 - Set-aside"))
+    
+    # set the order of the groups
+    data$Group <- factor(data$Group, levels = c("RCP6.5 (REF) - Multifunctional", "RCP6.5 (REF) - Set-aside", "RCP2.6 - Multifunctional", "RCP2.6 - Set-aside"))   
+    data_top$Group <- factor(data_top$Group, levels = c("RCP6.5 (REF) - Multifunctional", "RCP6.5 (REF) - Set-aside", "RCP2.6 - Multifunctional", "RCP2.6 - Set-aside"))   
+      if (grepl("timber", case_subcase, fixed = TRUE)) {
+        data$Category <- factor(data$Category, levels = c("Timber", "Clear_cut", "Other_management", "Retention", "Selection"), labels = c("Timber", "Clear cut", "Other management",  "Retention", "Selection"))
+      } else { 
+        data <- data %>% filter(Category != "Timber")
+        data$Category <- factor(data$Category, levels = c("Clear_cut", "Other_management", "Retention", "Selection"), labels = c("Clear cut", "Other management",  "Retention", "Selection"))
+      }
+    
+    figure <-
+      ggplot(data)+
+      geom_bar(aes(x = Scenario, y = PDFx100, fill = Category), colour = "black",  size = 0.3, width = 0.6, stat = "identity", position = position_stack(reverse = FALSE)) +
+      theme_minimal(base_size = 13) + # select the theme of the plot
+      theme(legend.position="right", 
+           legend.text = element_text(size = 10),
+           axis.text = element_text(size = 8),
+           axis.text.x = element_text(angle = 90),
+           axis.title = element_text(size = 10),
+           legend.title = element_text(size = 10)) +
+      scale_x_discrete(labels = label_scenario) + # assign the names to the labels
+      #ggtitle("Fraction of species loss in 2100 caused by EU internal forest management") +
+      xlab("Scenarios") + ylab("PDF%") +
+      #theme(plot.title = element_text(size = 12, face = "bold.italic")) +
+      #scale_x_discrete(labels = label_scenario) + # assign the names to the labels
+      #scale_fill_manual(values=c("coral","burlywood","cornflowerblue","darkseagreen")) + #, "darkred")) +
+      #scale_fill_manual(values=c("brown1","darkgreen","orange","darkkhaki"))+
+      scale_fill_brewer(palette = palette_name, direction = -1) +
+      #scale_fill_manual(values = pal) +
+      ylim(-0.02, round(max(data_top$PDFx100), digits = 2)) + 
+      geom_point(data = data_top, aes(x  = Scenario, y = PDFx100), color = "black", size = 1) +
+      theme(panel.border = element_rect(color="grey", fill=NA)) +
+      # geom_errorbar(data = marginal_CI, aes(x = Scenario, y = PDFx100, ymin = lower95, ymax = upper95), width = 0.1, color = "gray60", size = 0.8) +
+      facet_wrap(~ Group) +
+      labs(fill = "Forest use type") 
+    
+    figure
+    
+    ggsave(paste0(plots_path, "EUForest_", year, "_", palette_name, case_subcase, ".pdf"), width = 20, height = 16, units = "cm")
+    
+  }
