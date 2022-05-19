92cea249b448e416045fb3e1a5b5e2096f57f867
francesca-git <rosa@ifu.baug.ethz.ch>
2022-05-19 14:47:24 +0200

    Update

--- main.R
+++ main.R
@@ -4,7 +4,7 @@
 # Load the file and set the path for saving the output
 # March 2021
 # Author: Francesca Rosa
-
+####################
 ############################ SET WORKING DIRECTORY ################################
 
 setwd("C:/Users/Rosa/Documents/GitHub/forest-management") 
@@ -34,17 +34,17 @@ select <- dplyr::select
 ############################ INITIAL SETTINGS ######################################
 
   # General settings for the calculation of the impacts
-    marginal = FALSE         # TRUE or FALSE. Default IS TRUE. TRUE: the script loads the areas modelled by GLOBIOM with a marginal approach (imports and exports involve only intensive forest use, Plantation and Clear cut). FALSE: the script loads the areas modelled by GLOBIOM with an average approach (imports and exports involve all types of forest management). 
+    marginal = TRUE         # TRUE or FALSE. Default IS TRUE. TRUE: the script loads the areas modelled by GLOBIOM with a marginal approach (imports and exports involve only intensive forest use, Plantation and Clear cut). FALSE: the script loads the areas modelled by GLOBIOM with an average approach (imports and exports involve all types of forest management). 
     timber = FALSE          # TRUE or FALSE. FALSE = default setting which means that timber plantations are not included in the management practices considered part of EU clear cut areas have been allocated to Timber plantations). WARNING: This option is valid only if approach == "MG"
     CI = FALSE              # TRUE (confidence intervals are calculated) or FALSE (confidence intervals are not calculated)
 
   # Settings to define which .Rdata file to load for the response ratios and the z values 
     cutoff = TRUE           # TRUE (all raw RR > 1 are set to 1) or FALSE
-    vulnerability = TRUE    # TRUE (global impacts are quantified) or FALSE. Default = TRUE. 
+    vulnerability = FALSE    # TRUE (global impacts are quantified) or FALSE. Default = TRUE. 
     BS = TRUE               # If CI = TRUE and BS = TRUE, confidence intervals are quantified with bootstrapping. If CI = TRUE and BS = FALSE, confidence intervals are quantified with MonteCarlo simulation. 
                             # if CI = FALSE, no confidence intervals are quantified so it does not matter which value is assigned to BS.
     
-    subcase = "" # available options: "" = all species groups aggregated, "mammals" = results for mammals (aggregated), "birds" = results for birds (aggregated), "plants" = results for plants (aggregated)
+    subcase = "mammals" # available options: "" = all species groups aggregated, "mammals" = results for mammals (aggregated), "birds" = results for birds (aggregated), "plants" = results for plants (aggregated)
 
 ############################# SETTING THE LOCATION OF THE DIRECTORIES NEEDED TO RUN THE MODEL, AGGREGATE THE RESULTS AND PLOT THEM  ############################# 
 
@@ -134,7 +134,7 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
     
     source("./scripts/aggregation/aggregate_results.R")          # functions to convert the results of the model in a single .Rdata file
 
-    aggregate.results(results_path, file_rdata_path)     # aggregate_results.R - this function takes the results of the model calculation for a specific calculation setting
+    aggregate.results(results_path, file_rdata_path, file_label)     # aggregate_results.R - this function takes the results of the model calculation for a specific calculation setting
                                                          # defined by the code words at the beginning of this file (e.g. applying a cutoff and including timber plantations 
                                                          #, bootstrapping approach for the CI) and aggregate them as sum over the ecoregions or as total sums. 
 
