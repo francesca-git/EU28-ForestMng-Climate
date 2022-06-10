ea691a599060185e040554260c47bd3ef211a0bd
francesca-git <rosa@ifu.baug.ethz.ch>
2022-06-10 18:09:42 +0200

    Added an option to turn on the alternative (low-intensity) scenario so that the path of the areas is adapted and reaches the alternative areas

--- main.R
+++ main.R
@@ -34,7 +34,7 @@ select <- dplyr::select
 ############################ INITIAL SETTINGS ######################################
 
   # General settings for the calculation of the impacts
-    marginal = TRUE         # TRUE or FALSE. Default IS TRUE. TRUE: the script loads the areas modelled by GLOBIOM with a marginal approach (imports and exports involve only intensive forest use, Plantation and Clear cut). FALSE: the script loads the areas modelled by GLOBIOM with an average approach (imports and exports involve all types of forest management). 
+    marginal = FALSE         # TRUE or FALSE. Default IS TRUE. TRUE: the script loads the areas modelled by GLOBIOM with a marginal approach (imports and exports involve only intensive forest use, Plantation and Clear cut). FALSE: the script loads the areas modelled by GLOBIOM with an average approach (imports and exports involve all types of forest management). 
     timber = FALSE          # TRUE or FALSE. FALSE = default setting which means that timber plantations are not included in the management practices considered part of EU clear cut areas have been allocated to Timber plantations). WARNING: This option is valid only if approach == "MG"
     CI = FALSE              # TRUE (confidence intervals are calculated) or FALSE (confidence intervals are not calculated)
 
@@ -43,14 +43,14 @@ select <- dplyr::select
     vulnerability = TRUE    # TRUE (global impacts are quantified) or FALSE. Default = TRUE. 
     BS = TRUE               # If CI = TRUE and BS = TRUE, confidence intervals are quantified with bootstrapping. If CI = TRUE and BS = FALSE, confidence intervals are quantified with MonteCarlo simulation. 
                             # if CI = FALSE, no confidence intervals are quantified so it does not matter which value is assigned to BS.
-    
+    lowintensity_imports = TRUE   
+
     subcase = "" # available options: "" = all species groups aggregated, "mammals" = results for mammals (aggregated), "birds" = results for birds (aggregated), "plants" = results for plants (aggregated)
 
 ############################# SETTING THE LOCATION OF THE DIRECTORIES NEEDED TO RUN THE MODEL, AGGREGATE THE RESULTS AND PLOT THEM  ############################# 
 
   source("./scripts/set_directories.R")
 
-    
     
 ############################ PREPARATION OF THE AREAS ######################################
 
@@ -227,7 +227,7 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
 
       ################# MAP OF GLOBAL/EU IMPACTS #######################
           
-          id = "EUFootprint" # options: "EUForest", "EUFootprint" or "Global"
+          id = "EUForest" # options: "EUForest", "EUFootprint" or "Global"
           map = "PDF" # "PDF" 
           graph = "B-50" # "B-50" or "B-25-50", to be selected when id == "EUFootprint". "B-50" = the map will plot the following scenarios for 2100: Baseline, Multifunctional100% and Set-Aside100%
                           # "B-25-50" = = the map will plot the following scenarios for 2100: Baseline, Multifunctional50%, Multifunctional100%, SetAside50% and Set-Aside100%
