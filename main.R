9f085e21794aed62e197662c493c33c744de46f3
francesca-git <rosa@ifu.baug.ethz.ch>
2022-08-07 21:16:29 +0200

    Scripts for the new results

--- main.R
+++ main.R
@@ -7,8 +7,8 @@
 ####################
 ############################ SET WORKING DIRECTORY ################################
 
-#setwd("C:/Users/Rosa/Documents/GitHub/forest-management") 
-setwd("/home/frrosa/R/forest-management/") 
+setwd("C:/Users/Rosa/Documents/GitHub/forest-management") 
+#setwd("/home/frrosa/R/forest-management/") 
 
 ############################ LOAD LIBRARIES ################################
 
@@ -30,14 +30,14 @@ library(arsenal)
 library(MASS)
 
 select <- dplyr::select
-
+rename <- dplyr::rename
 
 ############################ INITIAL SETTINGS ######################################
 
   # General settings for the calculation of the impacts
-    marginal = FALSE         # TRUE or FALSE. Default IS TRUE. TRUE: the script loads the areas modelled by GLOBIOM with a marginal approach (imports and exports involve only intensive forest use, Plantation and Clear cut). FALSE: the script loads the areas modelled by GLOBIOM with an average approach (imports and exports involve all types of forest management). 
+    marginal = TRUE         # TRUE or FALSE. Default IS TRUE. TRUE: the script loads the areas modelled by GLOBIOM with a marginal approach (imports and exports involve only intensive forest use, Plantation and Clear cut). FALSE: the script loads the areas modelled by GLOBIOM with an average approach (imports and exports involve all types of forest management). 
     timber = FALSE          # TRUE or FALSE. FALSE = default setting which means that timber plantations are not included in the management practices considered part of EU clear cut areas have been allocated to Timber plantations). WARNING: This option is valid only if approach == "MG"
-    CI = TRUE              # TRUE (confidence intervals are calculated) or FALSE (confidence intervals are not calculated)
+    CI = FALSE              # TRUE (confidence intervals are calculated) or FALSE (confidence intervals are not calculated)
 
   # Settings to define which .Rdata file to load for the response ratios and the z values 
     cutoff = TRUE           # TRUE (all raw RR > 1 are set to 1) or FALSE
@@ -46,25 +46,11 @@ select <- dplyr::select
                             # if CI = FALSE, no confidence intervals are quantified so it does not matter which value is assigned to BS.
     lowintensity_imports = TRUE   
 
-    subcase = "" # available options: "" = all species groups aggregated, "mammals" = results for mammals (aggregated), "birds" = results for birds (aggregated), "plants" = results for plants (aggregated)
+    subcase = "plants" # available options: "" = all species groups aggregated, "mammals" = results for mammals (aggregated), "birds" = results for birds (aggregated), "plants" = results for plants (aggregated)
 
 ############################# SETTING THE LOCATION OF THE DIRECTORIES NEEDED TO RUN THE MODEL, AGGREGATE THE RESULTS AND PLOT THEM  ############################# 
 
   source("./scripts/set_directories.R")
-               
-    year = "2100" # select the year. WARNING: use a character string containing the year (e.g., "2100")
-
-source("./scripts/plotting/for_WBF/EU_barplots_WBF.R")
-source("./scripts/plotting/for_WBF/aggregate_wood.R")
-          width_height = c(21, 16)
-          EUFootprint.areas.barplot.EP(aggr_plot_path_areas, label_timber, file_label, plots_path, year, width_height)
-          EUFootprint.volume.barplot.EP(areas_base_path, csv_path, plots_path, label_timber, file_label, year, width_height)  
-
-          EU.areas.barplot.EP.dis(aggr_plot_path_areas, label_timber, file_label, plots_path, year, width_height)
-          EU.volume.barplot.EP.dis(areas_base_path, csv_path, plots_path, label_timber, file_label, year, width_height)  
-    
-          EU.barplot.EP.dis(csv_path, file_label, plots_path, year, width_height)
-          EUFootprint.barplot.EP(csv_path, file_label, plots_path, year, width_height)
     
 ############################ PREPARATION OF THE AREAS ######################################
 
@@ -130,7 +116,6 @@ source("./scripts/plotting/for_WBF/aggregate_wood.R")
           
       ################# PREPARE DATA FOR PLOTTING (aggregated .Rdata file -> .csv files for global impacts #######################
           
-          source("./scripts/plotting/map_areas.R")                       # functions to plot the impacts of global land use 
           create.csv.global.areas(file_rdata_path_areas, aggr_plot_path_areas, label_timber, year)   # areas_Rdata-to-csv.R
     
     
@@ -202,7 +187,7 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
     # Select the year to be plotted and if the exports are included or not 
           
     year = "2100" # select the year. WARNING: use a character string containing the year (e.g., "2100")
-    energy_exports = "EPnoex" # "EPex" or "noEPnoex" or "EPnoex-dis" ("EPnoex-dis is an additional option to plot the barplot of the EU footprint with a disaggregation of the forest management typed for imported wood biomass)
+    energy_exports = "EPnoex-all-dis" # "EPex" or "noEPnoex" or "EPnoex-dis" ("EPnoex-dis is an additional option to plot the barplot of the EU footprint with a disaggregation of the forest management typed for imported wood biomass)
 
       ################# GLOBAL TIME SERIES AND AREAS (DISAGGREGATED) #######################
           
@@ -212,11 +197,14 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
       ################# BARPLOT OF EU FOOTPRINT  #######################
           
           source("./scripts/plotting/EU_barplots.R")                     # functions to plot the impacts of EU
-          
           if (energy_exports == "ex") { EUfootprint.barplot.EP(csv_path, file_label, plots_path, year) # EU_barplots.R
             }else if(energy_exports == "EPnoex") { EUfootprint.barplot.EP(csv_path, file_label, plots_path, year)
               }else if(energy_exports == "noEPnoex") { EUfootprint.barplot.noEP(csv_path, file_label, plots_path, year)
-                }else if(energy_exports == "EPnoex-dis") { EUfootprint.barplot.EP.dis(csv_path, file_label, plots_path, year)}
+                }else if(energy_exports == "EPnoex-dis") { EUfootprint.barplot.EP.dis(csv_path, file_label, plots_path, year)
+                  }else if(energy_exports == "EPnoex-all-dis") {
+                    width_height = c(27, 15)
+                    EU.barplot.EP.all.dis(csv_path, file_label, plots_path, year, width_height)
+                  }
   
       ################# BARPLOT OF EU INTERNAL FOREST IMPACTS #######################
           
@@ -224,6 +212,20 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
             }else if(energy_exports == "EPnoex") { EUinternal.barplot.EPnoex(csv_path, file_label, plots_path, year)
             }else if(energy_exports == "noEPnoex") { EUinternal.barplot.noEPnoex(csv_path, file_label, plots_path, year)}
           
+      ################# BARPLOT OF EU DEMAND AREAS  #######################
+          
+          if(energy_exports == "EPnoex-all-dis") {
+                    width_height = c(27, 15)
+                    EU.areas.barplot.EP.dis(aggr_plot_path_areas, label_timber, file_label, plots_path, year, width_height)
+                  }
+          
+      ################# BARPLOT OF EU DEMAND HARVEST  #######################
+          
+          source("./scripts/plotting/plot_biomass-harvested.R")                     # functions to plot the impacts of EU
+          if(energy_exports == "EPnoex-all-dis") {
+                    width_height = c(27, 15)
+                    EU.volume.barplot.EP.dis(areas_base_path, csv_path, plots_path, label_timber, file_label, year, width_height)  
+                  }
           
       ################# GLOBAL TIME SERIES (AGGREGATED, WITH CI) #######################
           
@@ -245,7 +247,7 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
 
       ################# MAP OF GLOBAL/EU IMPACTS #######################
           
-          id = "EUForest" # options: "EUForest", "EUFootprint" or "Global"
+          id = "EUFootprint" # options: "EUForest", "EUFootprint" or "Global"
           map = "PDF" # "PDF" 
           graph = "B-50" # "B-50" or "B-25-50", to be selected when id == "EUFootprint". "B-50" = the map will plot the following scenarios for 2100: Baseline, Multifunctional100% and Set-Aside100%
                           # "B-25-50" = = the map will plot the following scenarios for 2100: Baseline, Multifunctional50%, Multifunctional100%, SetAside50% and Set-Aside100%
@@ -289,5 +291,29 @@ if (dir.exists(aggr_plot_path) == TRUE) {file.rename(aggr_plot_path, paste0(aggr
                             # results_CI - list of dataframes containing the sums over ecoregions per each year considering only the CI values (disaggregated per land use), columns in each df: Group, Forest_use, Management, all the land uses
                             # sums_CI - list of dataframes dataframe containing the sums over ecoregions per each year considering only the CI values (aggregated over the land uses), columns: Group, Forest_use, Management, Sum_upper, Sum_lower  
 
+
+########## PLOTS FOR THE WORLD BIODIVERSITY FORUM ###########
           
+year = "2100" # select the year. WARNING: use a character string containing the year (e.g., "2100")
+
+source("./scripts/plotting/for_WBF/EU_barplots_WBF.R")
+source("./scripts/plotting/plot_biomass-harvested.R")
+          width_height = c(21, 16)
+          EUFootprint.areas.barplot.EP(aggr_plot_path_areas, label_timber, file_label, plots_path, year, width_height)
+          EUFootprint.volume.barplot.EP(areas_base_path, csv_path, plots_path, label_timber, file_label, year, width_height)  
+
+          EU.areas.barplot.EP.dis(aggr_plot_path_areas, label_timber, file_label, plots_path, year, width_height)
+          EU.volume.barplot.EP.dis(areas_base_path, csv_path, plots_path, label_timber, file_label, year, width_height)  
+    
+          EU.barplot.EP.dis(csv_path, file_label, plots_path, year, width_height)
+          EUFootprint.barplot.EP(csv_path, file_label, plots_path, year, width_height)
+          
+            
+          id = "EUFootprint" # options: "EUForest", "EUFootprint" or "Global"
+          map = "PDF" # "PDF" 
+          graph = "B-50" # "B-50" or "B-25-50", to be selected when id == "EUFootprint". "B-50" = the map will plot the following scenarios for 2100: Baseline, Multifunctional100% and Set-Aside100%
+                          # "B-25-50" = = the map will plot the following scenarios for 2100: Baseline, Multifunctional50%, Multifunctional100%, SetAside50% and Set-Aside100%
+          
+          source("./scripts/plotting/for_WBF/map_PDF_multi.R")                   
+          # This other function is meant to be tailored according to what it is needed to be plotted.
           
